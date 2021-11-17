8ad0e3ab7710cfae8d28202272bec9c74f30b6ad
Justin Lowell <justin.lowell@navy.mil>
2021-11-17 12:40:48 -0500

    slight changes to title

--- justins-web/src/App.js
+++ justins-web/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Justin's Portfolio</h1>
+        <h1 className = "Title">Justin's Portfolio</h1>
       </header>
     </div>
   );
