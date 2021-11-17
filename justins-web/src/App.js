112b8340eb8b85f90ed7f639da6ed1d0a6cc5eac
Justin Lowell <justin.lowell@navy.mil>
2021-11-17 13:44:38 -0500

    Added react-bootstrap

--- justins-web/src/App.js
+++ justins-web/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
+import NavigationBar from './Pages/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className = "Title">Justin's Portfolio</h1>
-      </header>
-    </div>
+    <NavigationBar /> 
   );
 }
 
---
+++ justins-web/src/Pages/NavigationBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+/**
+ * @brief Builds the Navigation Bar using HTML
+ */
+export class NavigationBar extends React.Component {
+  render() {
+    return (
+      <Container>
+          <h1>Hi there sunshine</h1>
+      </Container>
+    );
+  }
+}
+
+// Making the NavigationBar importable
+export default NavigationBar;
