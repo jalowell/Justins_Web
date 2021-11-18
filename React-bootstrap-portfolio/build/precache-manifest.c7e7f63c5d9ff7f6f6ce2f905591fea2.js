9510c920385ccc24816842cef20c1448293b3ad8
Justin Lowell <justin.lowell@navy.mil>
2021-11-18 12:22:33 -0500

    Changed homepage

---
+++ React-bootstrap-portfolio/build/precache-manifest.c7e7f63c5d9ff7f6f6ce2f905591fea2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898fd7d260d6f4b9c94bec177776d794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7acc457fd2f4eb689a9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "89d547eff73e7e174fb6",
+    "url": "/static/css/main.b88ad8eb.chunk.css"
+  },
+  {
+    "revision": "a7acc457fd2f4eb689a9",
+    "url": "/static/js/2.22fd03ad.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/static/js/2.22fd03ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d547eff73e7e174fb6",
+    "url": "/static/js/main.79c0a586.chunk.js"
+  },
+  {
+    "revision": "70e28861d9e6066b0f97",
+    "url": "/static/js/runtime-main.2145828f.js"
+  },
+  {
+    "revision": "97e616cc99de779069e2e32d3a245019",
+    "url": "/static/media/about.97e616cc.jpeg"
+  },
+  {
+    "revision": "abc8753f598ded617ef4223435dd870a",
+    "url": "/static/media/appoint.abc8753f.png"
+  },
+  {
+    "revision": "a8ad463248554d21cf2daeae07589510",
+    "url": "/static/media/burger.a8ad4632.png"
+  },
+  {
+    "revision": "e997ce52760ca5c44a28ce64878914da",
+    "url": "/static/media/crypto.e997ce52.png"
+  },
+  {
+    "revision": "b5e762dea577f393f742750a4d1eb304",
+    "url": "/static/media/main1.b5e762de.jpeg"
+  },
+  {
+    "revision": "7fd423372d184d58e68111b6e05a24c2",
+    "url": "/static/media/resume.7fd42337.pdf"
+  },
+  {
+    "revision": "877510dccc51d796349474a4cbd08dce",
+    "url": "/static/media/seberianbuckwheat.877510dc.png"
+  },
+  {
+    "revision": "f087294e5a272d7b2a4feeb1be903501",
+    "url": "/static/media/todolist.f087294e.png"
+  },
+  {
+    "revision": "c7d81f746fa44d014697a7c685626b71",
+    "url": "/static/media/unheatedhoney.c7d81f74.png"
+  }
+]);
--- React-bootstrap-portfolio/build/service-worker.js
+++ React-bootstrap-portfolio/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-bootstrap-portfolio/precache-manifest.328fdb154ae2511fe1ad03f19d680b81.js"
+  "/precache-manifest.c7e7f63c5d9ff7f6f6ce2f905591fea2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-bootstrap-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
